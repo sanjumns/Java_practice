ee96d0ea3835c95fa1d93831d37cb918c83bba7c
Sanjay <sanjumns@yahoo.com>
2024-05-14 11:10:26 +0530

    update

---
+++ logical_prog/array/Demo.java
@@ -0,0 +1,13 @@
+class Demo 
+{
+	static int i=1;
+	public static void main(String[] args) 
+	{
+		while (i<=10)
+		{
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("Hello World!");
+	}
+}
