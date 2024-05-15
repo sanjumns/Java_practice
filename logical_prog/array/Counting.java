2ac4a88c781006042f6570a439f8cf20c0854404
Sanjay <sanjumns@yahoo.com>
2024-05-15 11:46:59 +0530

    update

---
+++ logical_prog/array/Counting.java
@@ -0,0 +1,14 @@
+class Counting 
+{
+	public static void main(String[] args) 
+	{
+		int[] a={1,2,3,4,5,6,7};
+		int count=0;
+		for (int i=0;i<a.length ;i++ )
+		{
+			count++;
+		}
+
+		System.out.println(count);
+	}
+}
